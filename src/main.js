4d6479c7821e3df81d90d54555830d028c1ba339
Takuma Hanatani <mail@potato4d.me>
2016-07-10 16:33:12 +0900

    Fixed CORS settings

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 const riot = require("riot");
 
-require("whatwg-fetch");
 require("./components/app.tag");
 require("./components/raw.tag");
 require("./components/post.tag");
 
 fetch("http://api.wp-app.org/wp-json/wp/v2/posts", {
 	method: "GET",
-	credentials: 'cors',
 	mode: 'cors'
 }).then( (data) => {
 	return data.json();
